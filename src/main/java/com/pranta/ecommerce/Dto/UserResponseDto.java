54a7c3ed20609d7104dcd4e107acf3574b4c7afa
Pranta Saha <prantasaha907@gmail.com>
2026-01-16 17:21:01 +0600

    User Response Dto Class

---
+++ src/main/java/com/pranta/ecommerce/Dto/UserResponseDto.java
@@ -0,0 +1,14 @@
+package com.pranta.ecommerce.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String email; 
+}
