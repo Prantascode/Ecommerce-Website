1069514440089a21b7aecb36d7ec5bb331ed7c10
Pranta Saha <prantasaha907@gmail.com>
2026-01-16 17:02:25 +0600

    Cart Repository

---
+++ src/main/java/com/pranta/ecommerce/CartRepository.java
@@ -0,0 +1,14 @@
+package com.pranta.ecommerce;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pranta.ecommerce.Entity.CartItem;
+import com.pranta.ecommerce.Entity.User;
+
+public interface CartRepository extends JpaRepository<CartItem,Long> {
+    List<CartItem> findByUser(User user);
+
+    void deleteByUser(User user);
+}
