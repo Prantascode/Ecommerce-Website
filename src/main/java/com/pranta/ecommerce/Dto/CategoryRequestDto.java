12e428fb1e0dcb7da2579ca2b0a612e27782c318
Pranta Saha <prantasaha907@gmail.com>
2026-04-19 22:13:02 +0600

    Category request and response dto

---
+++ src/main/java/com/pranta/ecommerce/Dto/CategoryRequestDto.java
@@ -0,0 +1,17 @@
+package com.pranta.ecommerce.Dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryRequestDto {
+    
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+}
