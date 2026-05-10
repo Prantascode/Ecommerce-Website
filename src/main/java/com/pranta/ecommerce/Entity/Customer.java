e2353263f029c1cd78fddd6b49ac997092afb8bf
Pranta Saha <prantasaha907@gmail.com>
2026-05-11 03:00:36 +0600

    Update validations

--- src/main/java/com/pranta/ecommerce/Entity/Customer.java
+++ src/main/java/com/pranta/ecommerce/Entity/Customer.java
@@ -7,8 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
