86b5e8bc9b3991ceb64851094e2782a94b6f69a4
Pranta Saha <prantasaha907@gmail.com>
2026-05-24 22:53:40 +0600

    Change to customer entity from user

--- src/main/java/com/pranta/ecommerce/Repository/CartRepository.java
+++ src/main/java/com/pranta/ecommerce/Repository/CartRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pranta.ecommerce.Entity.Cart;
 
 public interface CartRepository extends JpaRepository<Cart,Long> {
-    Optional<Cart> findByUserId(Long userId);
+    Optional<Cart> findByCustomerId(Long customerId);
 }
--- src/main/java/com/pranta/ecommerce/Repository/OrderRepository.java
+++ src/main/java/com/pranta/ecommerce/Repository/OrderRepository.java
@@ -2,17 +2,17 @@ package com.pranta.ecommerce.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.pranta.ecommerce.Entity.Customer;
 import com.pranta.ecommerce.Entity.Order;
 import com.pranta.ecommerce.Entity.Order.OrderStatus;
-import com.pranta.ecommerce.Entity.User;
 
 import java.util.List;
 
 
 public interface OrderRepository extends JpaRepository<Order,Long>{
     
-    List<Order> findByUser(User user);
-    List<Order> findAllByUserIdOrderByOrderDateDesc(Long userId);
+    List<Order> findByCustomer(Customer customer);
+    List<Order> findAllByCustomerIdOrderByOrderDateDesc(Long customerId);
     List<Order> findAll();
     List<Order> findByStatus(OrderStatus status);
 }
