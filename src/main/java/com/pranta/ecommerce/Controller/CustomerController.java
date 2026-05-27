6ea09f7013b0c107d547cab5cb8abde92a76faaa
Pranta Saha <prantasaha907@gmail.com>
2026-05-27 21:54:02 +0600

    Customer controller

--- src/main/java/com/pranta/ecommerce/Controller/CustomerController.java
+++ src/main/java/com/pranta/ecommerce/Controller/CustomerController.java
@@ -1,26 +1,45 @@
 package com.pranta.ecommerce.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import com.pranta.ecommerce.Dto.CustomerRequestDto;
 import com.pranta.ecommerce.Dto.CustomerResponseDto;
 import com.pranta.ecommerce.Service.CustomerService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Controller
-@RequestMapping("/api/customer")
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
-    
-    public final CustomerService customerService;
 
+    private final CustomerService customerService;
+
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/me")
-    public ResponseEntity<CustomerResponseDto> getMyProfile(Authentication authentication){
+    public ResponseEntity<CustomerResponseDto> getMyProfileDetails(Authentication authentication) {
+
         String email = authentication.getName();
-        return ResponseEntity.ok(customerService.getMyProfileDetails(email));
+
+        CustomerResponseDto response = customerService.getMyProfileDetails(email);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("/me")
+    public ResponseEntity<CustomerResponseDto> updateMyProfileDetails(
+            Authentication authentication,
+            @Valid @RequestBody CustomerRequestDto dto
+    ) {
+        String email = authentication.getName();
+
+        CustomerResponseDto response = customerService.updateMyProfileDetails(email, dto);
+
+        return ResponseEntity.ok(response);
     }
 }
