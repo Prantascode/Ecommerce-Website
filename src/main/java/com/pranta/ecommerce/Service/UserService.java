deed2fddfd9adf8be669e1b959141222a9f9677e
Pranta Saha <prantasaha907@gmail.com>
2026-05-28 22:16:21 +0600

    clear garbage

--- src/main/java/com/pranta/ecommerce/Service/UserService.java
+++ src/main/java/com/pranta/ecommerce/Service/UserService.java
@@ -9,9 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import com.pranta.ecommerce.Dto.UpdateEmailDto;
-import com.pranta.ecommerce.Dto.UserRequestDto;
 import com.pranta.ecommerce.Dto.UserResponseDto;
 import com.pranta.ecommerce.Entity.Customer;
 import com.pranta.ecommerce.Entity.User;
