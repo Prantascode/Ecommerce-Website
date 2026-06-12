de4dd833735499f3d3d5f6984a793f28b333ebaa
Pranta Saha <prantasaha907@gmail.com>
2026-06-12 12:35:29 +0600

    review dtos

---
+++ src/main/java/com/pranta/ecommerce/Dto/ReviewUpdateRequestDto.java
@@ -0,0 +1,18 @@
+package com.pranta.ecommerce.Dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewUpdateRequestDto {
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 5, message = "Rating must not exceed 5")
+    private Integer rating;
+    
+    private String comment;
+}
