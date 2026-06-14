b82fd32f630d76c0501d2da21f283eab693e604c
Pranta Saha <prantasaha907@gmail.com>
2026-06-14 15:46:22 +0600

    New Method

--- src/main/java/com/pranta/ecommerce/Repository/ReviewRepository.java
+++ src/main/java/com/pranta/ecommerce/Repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.pranta.ecommerce.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,5 +14,9 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 	boolean existsByCustomerAndProduct(Customer customer, Product product);
 
     List<Review> findByProduct(Product product);
+
+    Optional<Review> findByProductId(Long productId);
+
+    List<Review> findAllReviewsbyProductId(Long productId);
     
 }
