2bda68b720b19b8084a9e82e06e9df78d4545b8b
Pranta Saha <prantasaha907@gmail.com>
2026-06-19 17:31:07 +0600

    Find by name methods

--- src/main/java/com/pranta/ecommerce/Repository/BrandRepository.java
+++ src/main/java/com/pranta/ecommerce/Repository/BrandRepository.java
@@ -1,10 +1,14 @@
 package com.pranta.ecommerce.Repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pranta.ecommerce.Entity.Brand;
 
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
+    Optional<Brand> findByName(String name);
 }
--- src/main/java/com/pranta/ecommerce/Repository/CategoryRepository.java
+++ src/main/java/com/pranta/ecommerce/Repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.pranta.ecommerce.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pranta.ecommerce.Entity.Category;
 
+
 public interface CategoryRepository extends JpaRepository<Category,Long>{
     
+    Optional<Category> findByName(String name);
 }
