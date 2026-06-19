7df62b4c4722eecd1043b1b5c577f849dcfdd03a
Pranta Saha <prantasaha907@gmail.com>
2026-06-19 17:24:43 +0600

    Change Order Item service class to interface

--- src/main/java/com/pranta/ecommerce/Service/OrderItemService.java
+++ src/main/java/com/pranta/ecommerce/Service/OrderItemService.java
@@ -1,54 +1,12 @@
 package com.pranta.ecommerce.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
 import com.pranta.ecommerce.Dto.OrderItemResponseDto;
 import com.pranta.ecommerce.Entity.CartItem;
 import com.pranta.ecommerce.Entity.Order;
-import com.pranta.ecommerce.Entity.OrderItem;
 
-import com.pranta.ecommerce.Repository.OrderItemRepository;
-
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-@Service
-@RequiredArgsConstructor
-public class OrderItemService {
-
-    private final OrderItemRepository orderItemRepository;
-
-    @Transactional
-    public List<OrderItemResponseDto> convertCartItemToOrderItem(List<CartItem> cartItems, Order order) {
-        List<OrderItem> orderItems = new ArrayList<>();
-
-        for (CartItem cartItem : cartItems) {
-
-            OrderItem orderItem = new OrderItem();
-            orderItem.setProduct(cartItem.getProduct());
-            orderItem.setPrice(cartItem.getPrice());
-            orderItem.setQuantity(cartItem.getQuantity());
-            orderItem.setOrder(order);
-
-            orderItems.add(orderItem);
-
-        }
-        orderItemRepository.saveAll(orderItems);
-
-        return orderItems.stream()
-            .map(this::mapToResponse)
-            .toList();
-    }
-
-    private OrderItemResponseDto mapToResponse(OrderItem item) {
-        return new OrderItemResponseDto(
-                item.getId(),
-                item.getProduct().getId(),
-                item.getProduct().getName(),
-                item.getQuantity(),
-                item.getPrice(),
-                item.getOrder().getId()
-        );
-    }
+public interface OrderItemService {
+    
+    List<OrderItemResponseDto> convertCartItemToOrderItem(List<CartItem> cartItems, Order order);
 }
